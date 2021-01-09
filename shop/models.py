0023b2e7890ccc803b1891554ab441980afbd45f
Girish Mahamuni <43906251+mahamunigirish@users.noreply.github.com>
2021-01-10 01:14:30 +0530

    Update models.py

--- shop/models.py
+++ shop/models.py
@@ -1,22 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-
-# Create your models here
-
-# class UserProfileInfo(models.Model):
-#     user = models.OneToOneField(User,on_delete=models.CASCADE)
-
-# def __str__(self):
-#     return self.user.username
-    
-
-
-
-
-    
-
-
 class Product(models.Model):
     product_id = models.AutoField
     product_name = models.CharField(max_length=50)
@@ -81,16 +65,3 @@ class SlidesImgaes(models.Model):
 
 
 
-
-# Creating module for customer registration
-# class Customer(models.Model):
-#     customer_id = models.AutoField(primary_key=True)
-#     customer_name = models.CharField(max_length=70)
-    
-#     customer_email = models.EmailField(max_length=70, default="")
-#     # customer_phone = models.CharField(max_length=13, default="")
-#     customer_phone  =models.IntegerField( unique=True)
-#     password = models.CharField(max_length=500)
-
-#     def __str__(self):
-#         return self.customer_name
